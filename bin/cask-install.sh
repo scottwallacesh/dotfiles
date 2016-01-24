ac00cc227e8c4e144b29ab916998688436c792aa
Scott Wallace <scott@wallace.sh>
2016-01-24 10:01:44 +0000

    Initial check-in of 'brew cask install ...' script for auto-populating Macs with software.

---
+++ bin/cask-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+xargs brew cask install <<EOF
+    alfred
+    bbc-iplayer-downloads
+    beardedspice
+    dashlane
+    day-o
+    dropbox
+    cleanmymac
+    firefox
+    flux
+    gitx
+    gpgtools
+    google-chrome
+    google-drive
+    google-hangouts
+    gopro-studio
+    handbrake
+    iterm2
+    sonos
+    textmate
+    vlc
+    vmware-fusion
+    wireshark
+EOF

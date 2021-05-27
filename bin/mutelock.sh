224cbdcdd21784510a7132ab188db16982d761a2
Scott Wallace <scott@wallace.sh>
2021-05-27 09:15:43 +0100

    Update mutelock.sh

--- bin/mutelock.sh
+++ bin/mutelock.sh
@@ -4,16 +4,18 @@
    #https://unix.stackexchange.com/users/231160/nik-gnomic
 # Post on stackexchange: https://unix.stackexchange.com/questions/467456/how-to-mute-sound-when-xscreensaver-locks-screen/589614#589614
 
-gdbus monitor -e -d org.xfce.ScreenSaver | grep ActiveChanged --line-buffered |
-    while read line
-    do
-        case "$line" in
-            *"(true,)"*)
-                pactl set-sink-mute @DEFAULT_SINK@ on
-            ;;
-            *"(false,)"*)
-                pactl set-sink-mute @DEFAULT_SINK@ off
-            ;;
-        esac
-    done
+# Do nothing for a bit to ensure gdbus is up and awake, etc.
+sleep 300
+
+gdbus monitor -e -d org.xfce.ScreenSaver | grep -h ActiveChanged --line-buffered |
+  while read LINE; do
+    case "${LINE}" in
+      *"(true,)"*)
+        pactl set-sink-mute @DEFAULT_SINK@ on
+        ;;
+      *"(false,)"*)
+        pactl set-sink-mute @DEFAULT_SINK@ off
+        ;;
+    esac
+  done
 exit
